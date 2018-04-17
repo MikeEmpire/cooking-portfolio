e5adeb12fa2182f95bdbfa1b5ef91d9d7b94f7e1
Michael Olie <aolie94@gmail.com>
2018-04-17 16:27:14 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,68 @@
+# Vik's Portfolio
+
+This project is a MERN stack with Redux and file upload capabilities on the admin side. This project is for a friend who has a portfolio that he wishes to showcase and wants it to be handled by an admin.
+
+## Getting Started
+
+
+### Prerequisites
+
+Make sure you have node installed.
+
+### Installing
+
+```
+run npm i
+```
+
+Then go into the client folder and run:
+
+```
+npm i
+```
+
+From there go to back to the root folder and run:
+
+```
+npm start
+```
+
+## Running the tests
+
+Test will be coming soon
+
+### Break down into end to end tests
+
+Coming Soon
+
+### And coding style tests
+
+Coming Soon
+
+## Deployment
+Deployed with heroku
+
+## Built With
+
+* [React](https://reactjs.org/) - The web framework used
+* [Redux](https://redux.js.org/) - Predictable State Manager
+* [Node](https://nodejs.org/) - Web Framework
+* [Mongoose](mongoosejs.com/) - Store Data
+* [Create-react-app](https://github.com/facebook/create-react-app) - generate front-end client
+* [Express](https://expressjs.com/) - Backend-server
+
+## Versioning
+
+I used [Github](http://www.github.com) for versioning. For the versions available, see the [tags on this repository](https://github.com/swiftmike/project/tags).
+
+## Authors
+
+* **Michael Olie** - *Initial work* - [SwiftMike](https://github.com/SwiftMike)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* God Inspired
---
+++ server.js
@@ -0,0 +1,77 @@
+/**************************************/
+//*****// Dependencies
+/**************************************/
+
+// common utils
+const logger = require('morgan');
+const Promise = require('bluebird');
+const path = require("path");
+const http = require("http");
+const util = require('util');
+
+// Express dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors       = require('cors');
+
+// Data dependencies
+const mongoose   = require('mongoose');
+
+/**************************************/
+//*****// Variable Declaration
+/**************************************/
+
+let configDB = require('./config/databaseCreds');
+const app = express();
+const router = express.Router();
+const PORT = process.env.PORT || 5000;
+let db;
+
+/**************************************/
+//*****// Database Initialization
+/**************************************/
+
+mongoose.Promise = Promise;
+mongoose.connect(configDB.url, {
+  useMongoClient: true,
+  promiseLibrary: require('bluebird')
+});
+
+//Bind connection to error event (to get notification of connection errors)
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
+/**************************************/
+//*****// Express & Webpack Middleware
+/**************************************/
+
+app.use(bodyParser.urlencoded({extended: true}));   // parse application/x-www-form-urlencoded
+app.use(cors());                                    // Enable CORS from client-side
+app.use(bodyParser.json());                         // parse application/json
+app.use(logger('dev'));
+
+/**************************************/
+//*****// Initialize Routes
+/**************************************/
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  // Fallback for client-side routing
+  //  If no route is found, returns index.html instead of making non-existent request
+  app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+/**************************************/
+//*****// Initalize Port listen
+/**************************************/
+
+app.listen(PORT, () => {
+  console.log(`You are listening to Andre ${PORT}`);
+  console.log(`Mongo DB URI: ${configDB.url}`);
+});
